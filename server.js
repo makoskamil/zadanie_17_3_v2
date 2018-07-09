be477929e5d6add7145d38cb636f2a6ceac39675
Kamil Makoś <makos.kamil@o2.pl>
2018-07-09 21:11:38 +0200

    init_project

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var fs = require('fs')
+var bodyParser = require('body-parser');
+var app = express();
+var stringifyFile;
+
+
+app.use(bodyParser.json());
+
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data
+    res.send(data);
+    });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+    parsedFile = JSON.parse(stringifyFile);
+    parserFile.note = req.params.note;
+    stringifyFile = JSON.stringify(parsedFile);
+    
+    fs.writeFile('./test.json', stringifyFile, function(err) {
+        If (err) throw err;
+        console.log('file updated');
+        });
+    }
+);
+
+
+app.listen(3000);
